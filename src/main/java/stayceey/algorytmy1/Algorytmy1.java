c6a0a8937928b0c8bfe78bb1dab9eb5028e8be08
Robert Matuszewski <robert.matuszewski556@gmail.com>
2024-10-26 15:57:23 +0200

    newcomit

--- src/main/java/stayceey/algorytmy1/Algorytmy1.java
+++ src/main/java/stayceey/algorytmy1/Algorytmy1.java
@@ -1,8 +1,24 @@
 package stayceey.algorytmy1;
-
+import java.util.Scanner;
 public class Algorytmy1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner pierwszy = new Scanner(System.in);
+        Scanner drugi = new Scanner(System.in);
+        Scanner trzeci = new Scanner(System.in);
+        System.out.println("Podaj pierwszą liczbę.");
+        int a = pierwszy.nextInt();
+        System.out.println("Podaj drugą liczbę.");
+        int b = drugi.nextInt();
+        System.out.println("Podaj trzecią liczbę.");
+        int c = trzeci.nextInt();
+        int max = a;
+        if (b > max){
+            max = b;
+        }
+        if (c > max ){
+            max =c;
+        }
+        System.out.println("Największa liczba to: "+max);
     }
 }
